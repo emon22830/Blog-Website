128642b1cbe8de5a15fb0f3209e9123c045da9a3
emon22830 <mdemon22830@gmail.com>
2025-11-03 12:15:07 +0800

    first commit

---
+++ backend/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userScehma = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    
+
+})
